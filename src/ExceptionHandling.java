718941e852e0e11c522187d27d37d67b687da8ec
Amit <amit5yadav5762@gmail.com>
2024-10-16 21:21:29 +0530

    re staring the coding journey after long time
    some health issue (skin infection)

--- src/ExceptionHandling.java
+++ src/ExceptionHandling.java
@@ -2,7 +2,7 @@ public class ExceptionHandling {
     public static void main(String[] args) {
         System.out.println("amit yadav");
         try{
-            System.out.println(10/0);
+            System.out.println(10/0); //  can cause abnormal termination 
         }
         catch(Exception e) {
             e.printStackTrace(); // if we comment this so no exception is shown there
